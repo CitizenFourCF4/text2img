d3cec55689c9a7a231390c3d76d6cbac29386249
CitizenFourCF4 <mitya.ulyanov97@mail.ru>
2024-03-16 00:19:48 +0300

    Удалил несуществующую ссылку

--- frontend/src/Links.jsx
+++ frontend/src/Links.jsx
@@ -3,7 +3,6 @@ import {Routes, Route} from 'react-router-dom'
 import HomePage from './pages/home/home'
 import LoginPage from './pages/login/login'
 import RegisterPage from './pages/register/register'
-import ChatPage from './pages/chat/chat'
 import AuthProvider from './context/AuthProvider'
 
 
@@ -12,7 +11,6 @@ const Links = () => {
     <AuthProvider>
       <Routes>
         <Route path="/" element={<HomePage />}/>
-        <Route path="/chat/:id" element={<ChatPage />}/>
         <Route path="/login" element={<LoginPage />}/>
         <Route path="/register" element={<RegisterPage />}/>
       </Routes>
