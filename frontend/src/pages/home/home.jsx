941d7e0db6843e385f94cfbd71f765cae80b9f99
CitizenFourCF4 <mitya.ulyanov97@mail.ru>
2024-03-19 20:32:10 +0300

    Устранена ошибка с входом неавторизованного пользователя

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -1,25 +1,38 @@
-import React, {useState} from 'react'
+import React, {useState, useContext, useEffect} from 'react'
 import styles from './home.module.css'
 import Sidebar from '../../components/sidebar/sidebar'
 import Welcome from '../../components/welcome/welcome'
 import ChatContainer from '../../components/chatContainer/chatContainer'
+import AuthContext from '../../context/AuthContext'
+import { useNavigate } from "react-router-dom";
 
 
 const HomePage = () => {
-  
+
+  const [isAuthorized, SetIsAuthorized] = useState(false)
   const [selectedChat, setSelectedChat] = useState(undefined)
   const [colorMode, setColorMode] = useState('dark')
+  const {authTokens} = useContext(AuthContext)
+  const navigate = useNavigate()
+
+  useEffect(()=>{
+    if (authTokens)SetIsAuthorized(true)
+    else navigate('/login')
+  }, []);
+
   return (
-    <div className={styles.container} colorMode={colorMode}>
-        <Sidebar setSelectedChat={setSelectedChat} colorMode={colorMode} setColorMode={setColorMode}/>
+    <div  >
+      { isAuthorized && ( 
+        <div className={styles.container} colorMode={colorMode}>
+          <Sidebar setSelectedChat={setSelectedChat} colorMode={colorMode} setColorMode={setColorMode}/>
           {selectedChat 
             ?(
               <ChatContainer selectedChat={selectedChat} colorMode={colorMode}/>
-             )
+            )
             :(
               <Welcome colorMode={colorMode}/>
-             )
-          }      
+            )}      
+        </div>)}
     </div>
   )
 }
