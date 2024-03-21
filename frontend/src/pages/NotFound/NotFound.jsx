5b62b43802037debe7d5f706248d7a378a4a525e
Renat008 <kyzminrenat@mail.ru>
2024-03-21 20:11:36 +0300

    Update 404 page

--- frontend/src/pages/NotFound/NotFound.jsx
+++ frontend/src/pages/NotFound/NotFound.jsx
@@ -7,9 +7,10 @@ const NotFound = () => {
       <div className={styles.container}>
       <div className={styles.layout__box}>
         <div className={styles.layout__body}>
-          <h2 className={styles.auth__tagline}>Oops, you're funny!</h2>
+          <h2 className={styles.auth__tagline}>Oops...</h2>
+          <h1 className={styles.auth__tagline2}>Page Not Found</h1>
           <img 
-            src="/src/assets/404page.gif" width="500" height="500" alt="">
+            src="/src/assets/CodePen-404.gif" width="700" height="550" alt="">
           </img>
             <a href="/" className={styles.submitButton}> 
                 Go Back To Home
